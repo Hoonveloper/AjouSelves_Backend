b003236caddfc956478216d008ea81b3f6402a1f
devnak <showri0108@naver.com>
2022-03-27 21:58:15 +0900

    [EH] user post pw crypto

--- routes/user.js
+++ routes/user.js
@@ -1,38 +1,51 @@
 const express = require("express");
 const router = express.Router();
+const crypto = require('crypto');
 const DB = require('../database/db_info'); // DB 정보 가져오기
+const { promise } = require("../database/db_info");
 DB.connect();
-const usertag = 'INSERT INTO USER (userid, password, email, status, nickname, socialtype, sex, birth, address, account, create_at, profileLink) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)';
+const usertag = 'INSERT INTO USER (userid, password, email, status, nickname, socialtype, sex, birth, address, account, create_at, profileLink, salt) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)';
 
 //body-parser
 router.use(express.urlencoded({extended:true}));
 router.use(express.json());
 
+function createHashedPassword(plainPassword) {
+        const salt = crypto.randomBytes(64).toString('base64')
+        return [crypto.pbkdf2Sync(plainPassword, salt, 9999, 64, 'sha512').toString('base64'), salt];
+}
+
 // 형식에 맞게 json 파일로 넘겨주면 이를 user db에 넣어준다.
 router.post('/add', (req,res) => {
     const rebo = req.body;
-    const reqvalue = [rebo.userid, rebo.password, rebo.email, rebo.status, rebo.nickname, rebo.socialtype, rebo.sex, rebo.birth, rebo.address, rebo.account, rebo.create_at, rebo.profileLink];
+    const hasfun = createHashedPassword(rebo.password);
+    const salt = hasfun[1];
+    const password = hasfun[0];
+    const reqvalue = [rebo.userid, password, rebo.email, rebo.status, rebo.nickname, rebo.socialtype, rebo.sex, rebo.birth, rebo.address, rebo.account, rebo.create_at, rebo.profileLink, salt];
     DB.query(usertag,reqvalue, (err,result,fileds) => {
         if(err) console.log(err);
-        console.log("add success");
+        //console.log(salt);
         res.send(req.body);
     })
-   console.log("User add");
+   //console.log("User add");
 })
 
 router.get('/get/:id', (req,res) => {
     const number = req.params.id;
     DB.query('SELECT *FROM USER', (err,result,fileds)=>{
         if(err) alert("DB Get Error");
-        res.send(result[number]);    
+        res.json(result[number]);    
     })
 })
 
-router.get('/get/', (req,res) => {
+router.get('/get', (req,res) => {
     const number = req.params.id;
     DB.query('SELECT *FROM USER', (err,result,fileds)=>{
         if(err) alert("DB Get Error");
-        res.send(result);    
+        //console.log(result);
+        //console.log(JSON.stringify(result,null, '\n'));
+        res.send(result);
+        // 정렬해서 넘겨줄 필요가 있다.    
     })
 })
 
